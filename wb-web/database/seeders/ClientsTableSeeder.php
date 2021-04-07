7fcc67625026e2f4a200ae0e4609f7b3360c6db5
Arne Naessens <arnenaessens@hotmail.com>
2021-04-07 23:08:49 +0200

    Add clients seeder

---
+++ wb-web/database/seeders/ClientsTableSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Seeder;
+
+class ClientsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('clients')->insert([
+            'name' => 'Assy-Soft',
+            'phone' => '+32471144141',
+            'fax' => '056255114',
+            'vat' => 'BE5142 52 25 22'
+        ]);
+    }
+}
